d761cc674cc541a324eda36709ce3adb341b9389
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2026-09-09 23:59:23 +0530

    adding gateway modeule

--- backend/src/gateway/gateway.module.ts
+++ backend/src/gateway/gateway.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
+import { PolicyModule } from '../policy/policy.module';
+import { RiskModule } from '../risk/risk.module';
 
 @Module({
+  imports: [PolicyModule, RiskModule],
   controllers: [GatewayController],
-  providers: [GatewayService]
+  providers: [GatewayService],
 })
 export class GatewayModule {}
