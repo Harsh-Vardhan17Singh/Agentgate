f7b4205806fd8f3e8e811f58db863ecd2ae1437c
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2026-09-24 12:11:12 +0530

    adding agent activation and deactivation endpoint

--- backend/src/auth/agent.controller.ts
+++ backend/src/auth/agent.controller.ts
@@ -2,6 +2,7 @@ import {
   Controller,
   Get,
   Param,
+  Patch,
 } from '@nestjs/common';
 
 import { AgentService } from './agent.service';
@@ -22,5 +23,25 @@ export class AgentController {
     @Param('agentId') agentId: string,
   ) {
     return this.agentService.getAgent(agentId);
+  }
+
+  @Patch(':agentId/activate')
+  activateAgent(
+    @Param('agentId') agentId: string,
+  ) {
+    return this.agentService.setAgentStatus(
+      agentId,
+      true,
+    );
+  }
+
+  @Patch(':agentId/deactivate')
+  deactivateAgent(
+    @Param('agentId') agentId: string,
+  ) {
+    return this.agentService.setAgentStatus(
+      agentId,
+      false,
+    );
   }
 }
